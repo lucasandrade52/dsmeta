dba235b633768fd03d2dc5370375fd9751910239
Lucas Nogueira <lucasandrade_52@hotmail.com>
2022-11-15 20:54:36 -0300

    create first component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
+import NotificationButton from "./components/NotificationButton";
+
 function App() {
   return (
     <div className="App">
       <h1>Testando</h1>
+      <NotificationButton />
     </div>
   );
 }
